c4cbeeac51dd18e4efe6aac274ed8a0d1cccd5ed
bsaitkulov <bsaitkulov@gmail.com>
2023-04-04 19:06:21 +0400

    feature/GSPINT-1221

---
+++ src/VisaPayment.java
@@ -0,0 +1,10 @@
+public class VisaPayment {
+
+    public void payViaVisa(String bank){
+        System.out.println("Some payment from: " + bank);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
